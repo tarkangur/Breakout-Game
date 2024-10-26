e049d623b7cd1f175d9b7b79a463889bcd6e4bee
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-10-27 00:10:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import turtle as t
+from paddle import Paddle
 
 
-turtle = t.Turtle()
 screen = t.Screen()
-
 screen.setup(width=1.0, height=1.0)
+screen.bgcolor("white")
+screen.title("Breakout")
+screen.tracer(0)
+
+paddle = Paddle((0, -350))
+
+screen.listen()
+
+screen.onkeypress(paddle.go_right, "Right")
+screen.onkeypress(paddle.go_left, "Left")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
 t.mainloop()
