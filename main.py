41cc66e8328f8ee31d46b39f59227d3164bb102a
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-10-29 21:50:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import time
 import turtle as t
 from paddle import Paddle
+from ball import Ball
 
 
 screen = t.Screen()
 screen.setup(width=1.0, height=1.0)
-screen.bgcolor("white")
+screen.bgcolor("black")
 screen.title("Breakout")
 screen.tracer(0)
 
 paddle = Paddle((0, -350))
+ball = Ball((0, -330))
 
 screen.listen()
 
@@ -18,7 +21,11 @@ screen.onkeypress(paddle.go_left, "Left")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.05)
     screen.update()
+    ball.move()
+    if ball.ycor() < - 340 and ball.distance(paddle) < 200:
+        ball.paddle_hit(paddlex=paddle.xcor())
 
 
 t.mainloop()
