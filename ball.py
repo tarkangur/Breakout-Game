5462694508fa23c7be0db8d6e536bf0649179331
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-10-29 12:55:30 +0300

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+import random
+
+STARTING_HEAD = [i for i in range(9, 171)]
+
+
+class Ball(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.color("grey")
+        self.shape("circle")
+        STARTING_HEAD.pop(STARTING_HEAD.index(90))
+        self.setheading(random.choice(STARTING_HEAD))
+        self.speed(3)
+        self.goto(position)
+
+    def move(self):
+        x = self.xcor()
+        y = self.ycor()
+
+        if y + 10 > 475:
+            self.setheading(self.heading() * -1)
+        elif x + 10 > 760 or x - 10 < -760:
+            self.setheading((self.heading() + 180) * -1)
+        self.forward(10)
