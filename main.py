de22d7354e710be17e0f0aa60196f390007e95e9
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-11-03 18:48:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,30 @@ screen.listen()
 screen.onkeypress(paddle.go_right, "Right")
 screen.onkeypress(paddle.go_left, "Left")
 
-game_is_on = True
 
+def check_collision_with_blocks():
+    for block in blocks.all_blocks:
+        right, left, top, bot = blocks.block_wall(block)
+
+        if ball.distance(right) < 20 or ball.distance(left) < 20:
+            ball.setheading((ball.heading() + 180) * -1)
+            block.hideturtle()
+            blocks.all_blocks.remove(block)
+        elif ball.distance(top) < 20 or ball.distance(bot) < 20:
+            ball.setheading(ball.heading() * -1)
+            block.hideturtle()
+            blocks.all_blocks.remove(block)
+
+
+game_is_on = True
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(0.02)
     screen.update()
     ball.move()
     if ball.ycor() < - 340 and ball.distance(paddle) < 200:
         ball.paddle_hit(paddlex=paddle.xcor())
+
+    check_collision_with_blocks()
 
 
 t.mainloop()
